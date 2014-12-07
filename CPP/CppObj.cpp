b3d6441decb1228831e4024f6ad9848b64f73901
yangzq <732142068@qq.com>
2014-12-07 19:50:53 +0800

    完成工程初始架构

---
+++ CPP/CppObj.cpp
@@ -0,0 +1,14 @@
+//
+//  Cam.cpp
+//  DesingPatterns
+//
+//  Created by zhiquan on 14-12-7.
+//  Copyright (c) 2014年 Chee. All rights reserved.
+//
+
+#include "CppObj.h"
+
+int CppObj::showCam(){
+    
+    return 0;
+}
---
+++ CPP/ObjcppObj.h
@@ -0,0 +1,15 @@
+//
+//  ObjCVImg.h
+//  DesingPatterns
+//
+//  Created by zhiquan on 14-12-7.
+//  Copyright (c) 2014年 Chee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjcppObj : NSObject
+
++ (void) objShowCam;
+
+@end
